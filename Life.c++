82033398f0e6b8f3dc86a12eb1886f806721d6dd
Brian Jenkins <baj645@cs.utexas.edu>
2014-11-23 15:54:41 -0600

    First commit

---
+++ Life.c++
@@ -0,0 +1,18 @@
+// --------
+// includes
+// --------
+
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <cstring>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "Life.h"
+
+using namespace std;
+
+// -------------
+// abstract cell
+// -------------
---
+++ Life.h
@@ -0,0 +1,51 @@
+#ifndef Life_h
+#define Life_h
+
+// --------
+// includes
+// --------
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// ------------
+// AbstractCell
+// ------------
+
+class AbstractCell{
+	
+};
+
+// ----
+// Cell
+// ----
+
+class Cell{
+
+};
+
+// ----------
+// ConwayCell
+// ----------
+
+class ConwayCell{
+
+};
+
+// -----------
+// FredkinCell
+// -----------
+
+class FredkinCell{
+
+};
+
+// -------
+// Life<T>
+// -------
+
+class Life<T>{
+
+};
